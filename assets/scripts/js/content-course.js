3c8d452e2f81c76508952fe31b6f29e7b2cf34b2
keithpetrillo <keithpetrillo@gmail.com>
2020-09-23 11:15:47 -0400

    Added condition for single course magellan jquery

--- assets/scripts/js/content-course.js
+++ assets/scripts/js/content-course.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function ($) {
-    const courseMagellan = new Foundation.Magellan($('#course-magellan'), {
-        offset: 80,
-        threshold: 100
-    });
+    // todo: I'm not sure this is the best way to validate this
+    if ( jQuery('body').hasClass('page-template-template-course') ) {
+        const courseMagellan = new Foundation.Magellan($('#course-magellan'), {
+            offset: 80,
+            threshold: 100
+        });
+    }
 })
