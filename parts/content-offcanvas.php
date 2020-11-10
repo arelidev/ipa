b48cbbf6995ceaee0fc3435d9e2ed6b0f674dd9b
keithpetrillo <keithpetrillo@gmail.com>
2020-11-10 11:29:05 -0500

    Updated off-canvas
    https://areligroup.atlassian.net/browse/IP-48

--- parts/content-offcanvas.php
+++ parts/content-offcanvas.php
@@ -7,12 +7,31 @@
 ?>
 
 <div class="off-canvas position-right" id="off-canvas" data-off-canvas>
-	<?php joints_off_canvas_nav(); ?>
 
-	<?php if ( is_active_sidebar( 'offcanvas' ) ) : ?>
+    <div class="grid-x grid-padding-x grid-padding-y align-middle">
 
-		<?php dynamic_sidebar( 'offcanvas' ); ?>
+        <div class="shrink cell">
+            <a href="<?= get_field( 'magento_login_url', 'options' ); ?>" class="button hollow small" style="margin-bottom: 0;">
+		        <?= __( 'My Account', 'ipa' ); ?>
+            </a>
+        </div>
+        <div class="shrink cell">
+            <a href="<?= get_field( 'magento_cart_url', 'options' ); ?>">
+                <i class="far fa-shopping-cart"></i>
+            </a>
+        </div>
+        <div class="auto cell text-right">
+            <button type="button" data-toggle="off-canvas">CLOSE <i class="fas fa-times"></i></button>
+        </div>
+    </div>
 
-	<?php endif; ?>
+    <div class="grid-x">
+        <div class="cell">
+			<?php joints_off_canvas_nav(); ?>
 
+			<?php if ( is_active_sidebar( 'offcanvas' ) ) : ?>
+				<?php dynamic_sidebar( 'offcanvas' ); ?>
+			<?php endif; ?>
+        </div>
+    </div>
 </div>
