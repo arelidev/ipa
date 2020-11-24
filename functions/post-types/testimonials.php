a63ef88f5a303f624539aff49c5490e0e9ad53c5
keithpetrillo <keithpetrillo@gmail.com>
2020-11-24 14:27:02 -0500

    Add: ACF Json assets Add: testimonials category taxonomy Update: required plugin assets Remove: Redux Framework as a required plugin

--- functions/post-types/testimonials.php
+++ functions/post-types/testimonials.php
@@ -31,26 +31,70 @@ function testimonials_post_type() {
 		'items_list_navigation' => __( 'Testimonials list navigation', 'ipa' ),
 		'filter_items_list'     => __( 'Filter Testimonials list', 'ipa' ),
 	);
-	$args = array(
-		'label'                 => __( 'Testimonial', 'ipa' ),
-		'description'           => __( 'Customer testimonials', 'ipa' ),
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'thumbnail' ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 20,
-		'menu_icon'             => 'dashicons-format-quote',
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => false,
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => true,
-		'capability_type'       => 'page',
+	$args   = array(
+		'label'               => __( 'Testimonial', 'ipa' ),
+		'description'         => __( 'Customer testimonials', 'ipa' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'          => array( 'testimonials_taxonomy' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'menu_position'       => 20,
+		'menu_icon'           => 'dashicons-format-quote',
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => true,
+		'can_export'          => true,
+		'has_archive'         => false,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
 	);
 	register_post_type( 'Testimonials', $args );
 
 }
+
 add_action( 'init', 'testimonials_post_type', 0 );
+
+// Register Custom Taxonomy
+function testimonials_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Categories', 'Taxonomy General Name', 'ipa' ),
+		'singular_name'              => _x( 'Category', 'Taxonomy Singular Name', 'ipa' ),
+		'menu_name'                  => __( 'Category', 'ipa' ),
+		'all_items'                  => __( 'All Categories', 'ipa' ),
+		'parent_item'                => __( 'Parent Category', 'ipa' ),
+		'parent_item_colon'          => __( 'Parent Category:', 'ipa' ),
+		'new_item_name'              => __( 'New Category Name', 'ipa' ),
+		'add_new_item'               => __( 'Add New Category', 'ipa' ),
+		'edit_item'                  => __( 'Edit Category', 'ipa' ),
+		'update_item'                => __( 'Update Category', 'ipa' ),
+		'view_item'                  => __( 'View Category', 'ipa' ),
+		'separate_items_with_commas' => __( 'Separate Categories with commas', 'ipa' ),
+		'add_or_remove_items'        => __( 'Add or remove Categories', 'ipa' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'ipa' ),
+		'popular_items'              => __( 'Popular Categories', 'ipa' ),
+		'search_items'               => __( 'Search Categories', 'ipa' ),
+		'not_found'                  => __( 'Not Found', 'ipa' ),
+		'no_terms'                   => __( 'No Categories', 'ipa' ),
+		'items_list'                 => __( 'Categories list', 'ipa' ),
+		'items_list_navigation'      => __( 'Categories list navigation', 'ipa' ),
+	);
+	$args   = array(
+		'labels'            => $labels,
+		'hierarchical'      => false,
+		'public'            => true,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'show_in_nav_menus' => false,
+		'show_tagcloud'     => false,
+		'rewrite'           => false,
+		'show_in_rest'      => true,
+	);
+	register_taxonomy( 'testimonials_taxonomy', array( 'Testimonials' ), $args );
+
+}
+
+add_action( 'init', 'testimonials_taxonomy', 0 );
--- functions/tgm/tgm-init.php
+++ functions/tgm/tgm-init.php
@@ -41,57 +41,52 @@ function ipa_register_required_plugins() {
 	 */
 	$plugins = array(
 		array(
-			'name'      => 'Redux Framework',
-			'slug'      => 'redux-framework',
-			'required'  => true,
+			'name'     => 'Social Icons',
+			'slug'     => 'social-icons',
+			'required' => true,
 		),
 		array(
-			'name'      => 'Social Icons',
-			'slug'      => 'social-icons',
-			'required'  => true,
+			'name'     => 'Yoast SEO',
+			'slug'     => 'wordpress-seo',
+			'required' => true,
 		),
 		array(
-			'name'      => 'Yoast SEO',
-			'slug'      => 'wordpress-seo',
-			'required'  => true,
+			'name'     => 'Contact Form 7',
+			'slug'     => 'contact-form-7',
+			'required' => true,
 		),
 		array(
-			'name'      => 'Contact Form 7',
-			'slug'      => 'contact-form-7',
-			'required'  => true,
+			'name'               => 'WP Migrate DB Pro',
+			'slug'               => 'wp-migrate-db-pro',
+			'source'             => get_template_directory() . '/functions/tgm/plugins/wp-migrate-db-pro.zip',
+			'required'           => false,
+			'version'            => '',
+			'force_activation'   => false,
+			'force_deactivation' => false,
+			'external_url'       => '',
+			'is_callable'        => '',
 		),
 		array(
-			'name'               => 'WP Migrate DB Pro', // The plugin name.
-			'slug'               => 'wp-migrate-db-pro', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/functions/tgm/plugins/wp-migrate-db-pro.zip', // The plugin source.
-			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+			'name'               => 'Advanced Custom Fields PRO',
+			'slug'               => 'advanced-custom-fields-pro',
+			'source'             => get_template_directory() . '/functions/tgm/plugins/advanced-custom-fields-pro.zip',
+			'required'           => true,
+			'version'            => '',
+			'force_activation'   => false,
+			'force_deactivation' => false,
+			'external_url'       => '',
+			'is_callable'        => '',
 		),
 		array(
-			'name'               => 'Advanced Custom Fields PRO', // The plugin name.
-			'slug'               => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/functions/tgm/plugins/advanced-custom-fields-pro.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),
-		array(
-			'name'               => 'WPBakery Page Builder', // The plugin name.
-			'slug'               => 'js_composer', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/functions/tgm/plugins/js_composer.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+			'name'               => 'WPBakery Page Builder',
+			'slug'               => 'js_composer',
+			'source'             => get_template_directory() . '/functions/tgm/plugins/js_composer.zip',
+			'required'           => true,
+			'version'            => '',
+			'force_activation'   => false,
+			'force_deactivation' => false,
+			'external_url'       => '',
+			'is_callable'        => '',
 		),
 	);
 
@@ -105,16 +100,26 @@ function ipa_register_required_plugins() {
 	 * Only uncomment the strings in the config array if you want to customize the strings.
 	 */
 	$config = array(
-		'id'           => 'ipa',                   // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'parent_slug'  => 'themes.php',            // Parent menu slug.
-		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => true,                    // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
+		'id'           => 'ipa',
+		// Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',
+		// Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins',
+		// Menu slug.
+		'parent_slug'  => 'themes.php',
+		// Parent menu slug.
+		'capability'   => 'edit_theme_options',
+		// Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,
+		// Show admin notices or not.
+		'dismissable'  => true,
+		// If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',
+		// If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => true,
+		// Automatically activate plugins after installation or not.
+		'message'      => '',
+		// Message to output right before the plugins table.
 	);
 
 	tgmpa( $plugins, $config );
