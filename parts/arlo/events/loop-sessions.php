570c190b05e5a2eb4c891136ec4337075c3a7ace
Keith Petrillo <keith@areli.com>
2022-10-04 14:05:54 -0400

    Removed open availability

--- parts/arlo/events/loop-sessions.php
+++ parts/arlo/events/loop-sessions.php
@@ -4,14 +4,6 @@
 		$isFull = get_sub_field('isfull');
 		?>
         <div class="course-card-session">
-            <p>
-				<?php if ($isFull) : ?>
-                    <span class="label secondary"><?= __('class is full', 'ipa'); ?></span>
-				<?php else : ?>
-                    <span class="label primary"><?= __('open availability', 'ipa'); ?></span>
-				<?php endif; ?>
-            </p>
-
             <p class="course-card-name">
                 <b><?php get_template_part('parts/arlo/events/loop-session', 'name'); ?></b>
             </p>
