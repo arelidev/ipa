3338806be8d9cd56ceed59aa85ef49a522fe7ec4
Keith Petrillo <keith@areli.com>
2022-11-04 15:19:47 -0400

    Updated times

--- parts/arlo/events/loop-session-datetime.php
+++ parts/arlo/events/loop-session-datetime.php
@@ -3,12 +3,13 @@ $stateDateTime = get_sub_field('startdatetime');
 $stateDateTimezone = get_sub_field('starttimezoneabbr');
 $endDateTime = get_sub_field('enddatetime');
 $endDateTimezone = get_sub_field('endtimezoneabbr');
+
+$start_date = get_date_time($stateDateTime, $stateDateTimezone, get_option('date_format'));
+$state_time = get_date_time($stateDateTime, $stateDateTimezone, get_option('time_format'));
+$end_date = get_date_time($endDateTime, $endDateTimezone, get_option('date_format'));
+$end_time = get_date_time($endDateTime, $endDateTimezone, get_option('time_format'));
 ?>
 <span data-tooltip data-allow-html="true" tabindex="1"
-      title="
-        Starts on <?= date(get_option('date_format'), strtotime($stateDateTime)); ?> at <?= get_date_time($stateDateTime, $stateDateTimezone, get_option('time_format')); ?>
-        <br/>
-        End on <?= date(get_option('date_format'), strtotime($endDateTime)); ?> at <?= get_date_time($endDateTime, $endDateTimezone, get_option('time_format')); ?>
-">
-<?= date(get_option('date_format'), strtotime($stateDateTime)); ?> - <?= date(get_option('date_format'), strtotime($endDateTime)); ?>
+      title="Starts on <?= $start_date; ?> at <?= $state_time; ?><br/>End on <?= $end_date; ?> at <?= $end_time; ?>">
+      <?= $start_date; ?> - <?= $end_date; ?>
 </span>
--- parts/arlo/events/loop-session-time.php
+++ parts/arlo/events/loop-session-time.php
@@ -3,14 +3,14 @@ $stateDateTime = get_sub_field('startdatetime');
 $stateDateTimezone = get_sub_field('starttimezoneabbr');
 $endDateTime = get_sub_field('enddatetime');
 $endDateTimezone = get_sub_field('endtimezoneabbr');
+
+$start_date = get_date_time($stateDateTime, $stateDateTimezone, get_option('date_format'));
+$state_time = get_date_time($stateDateTime, $stateDateTimezone, get_option('time_format'));
+$end_date = get_date_time($endDateTime, $endDateTimezone, get_option('date_format'));
+$end_time = get_date_time($endDateTime, $endDateTimezone, get_option('time_format'));
 ?>
-<span data-tooltip data-allow-html="true" tabindex="1"
-      title="
-      <?= date(get_option('date_format'), strtotime($stateDateTime)); ?>
-      -
-      <?= date(get_option('date_format'), strtotime($endDateTime)); ?>
-">
-	<?= __('Starts at', 'ipa'); ?> <?= get_date_time($stateDateTime, $stateDateTimezone, get_option('time_format')); ?>
+<span data-tooltip data-allow-html="true" tabindex="1" title=" <?= $start_date; ?> - <?= $end_date; ?>">
+	<?= __('Starts at', 'ipa'); ?> <?= $state_time; ?>
     <br/>
-    <?= __('Ends at', 'ipa'); ?> <?= get_date_time($endDateTime, $endDateTimezone, get_option('time_format')); ?>
+    <?= __('Ends at', 'ipa'); ?> <?= $end_time; ?>
 </span>
