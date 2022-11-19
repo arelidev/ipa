9fae8b098160688cf8a882c7daba6dd94cf28c3c
Keith Petrillo <keith@areli.com>
2022-11-18 20:41:22 -0500

    Added placeholder image to presenter loop

--- parts/arlo/events/loop-presenters.php
+++ parts/arlo/events/loop-presenters.php
@@ -1,45 +1,49 @@
 <?php
-$post = isset($args['post']) ? (int)$args['post'] : get_the_ID();
+$post         = isset( $args['post'] ) ? (int) $args['post'] : get_the_ID();
 $disable_link = $args['disable_link'] ?? false;
 
-while (have_rows('presenters', $post)) : the_row();
-	$name = get_sub_field('name');
-	$presenter = get_sub_field('linked_presenter');
-    $users = false;
+while ( have_rows( 'presenters', $post ) ) : the_row();
+	$name      = get_sub_field( 'name' );
+	$presenter = get_sub_field( 'linked_presenter' );
+	$users     = false;
 
-    if ($presenter) {
-	    $users = get_users(array(
-		    'meta_query' => array(
-			    array(
-				    'key' => 'arlo_presenter_profile',
-				    'value' => $presenter,
-			    )
-		    )
-	    ));
-    }
+	if ( $presenter ) {
+		$users = get_users( array(
+			'meta_query' => array(
+				array(
+					'key'   => 'arlo_presenter_profile',
+					'value' => $presenter,
+				)
+			)
+		) );
+	}
 
-	if ($users) :
-		foreach ($users as $user) :
-			$profileImage = get_field('profile_image', 'user_' . $user->ID);
+	if ( $users ) :
+		foreach ( $users as $user ) :
+			$profileImage = get_field( 'profile_image', 'user_' . $user->ID );
 			?>
-
-			<?php if ($disable_link !== true) : ?>
-                <a href="<?= home_url(); ?>/profile-member/<?= acf_slugify($name); ?>">
-		    <?php endif; ?>
-
-            <img src="<?= wp_get_attachment_image_url($profileImage); ?>"
-                 class="course-card-trainer"
-                 alt="<?= $name ?>"
-                 data-tooltip tabindex="1"
-                 title="<?= $name; ?>">
-
-            <?php if ($disable_link !== true) : ?></a><?php endif; ?>
-
+			<?php if ( $disable_link !== true ) : ?>
+                <a href="<?= home_url(); ?>/profile-member/<?= acf_slugify( $name ); ?>">
+            <?php endif; ?>
+            <?php if ($profileImage) : ?>
+                <img src="<?= wp_get_attachment_image_url( $profileImage ); ?>"
+                     class="course-card-trainer"
+                     alt="<?= $name ?>"
+                     data-tooltip tabindex="1"
+                     title="<?= $name; ?>">
+             <?php else : ?>
+                <img src="<?= get_placeholder_image(); ?>"
+                     class="course-card-trainer"
+                     alt="<?= $name ?>"
+                     data-tooltip tabindex="1"
+                     title="<?= $name; ?>">
+             <?php endif; ?>
+			<?php if ( $disable_link !== true ) : ?></a><?php endif; ?>
 		<?php endforeach; ?>
 	<?php else : ?>
 
-		<?php if ($disable_link !== true) : ?>
-            <a href="<?= home_url(); ?>/profile-member/<?= acf_slugify($name); ?>">
+		<?php if ( $disable_link !== true ) : ?>
+            <a href="<?= home_url(); ?>/profile-member/<?= acf_slugify( $name ); ?>">
 		<?php endif; ?>
 
         <img src="<?= get_placeholder_image(); ?>"
@@ -48,7 +52,7 @@ while (have_rows('presenters', $post)) : the_row();
              data-tooltip tabindex="1"
              title="<?= $name; ?>">
 
-		<?php if ($disable_link !== true) : ?></a><?php endif; ?>
-    
+		<?php if ( $disable_link !== true ) : ?></a><?php endif; ?>
+
 	<?php endif; ?>
 <?php endwhile; ?>
