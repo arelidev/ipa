4ccbd084f7c3f7e5063847c23c1f0d750b3533a6
Keith Petrillo <keith@areli.com>
2023-07-10 18:11:03 -0400

    Updated header position styling

--- assets/scripts/js/hero-spacer.js
+++ assets/scripts/js/hero-spacer.js
@@ -1,14 +1,14 @@
-jQuery(document).ready(function () {
-    adjust_top_bar_spacer();
-
-});
-
-jQuery(window).resize(function () {
-    adjust_top_bar_spacer();
-});
-
-function adjust_top_bar_spacer() {
-    // todo: this should be more dynamically chosen
-    let hero = jQuery('.hero');
-    hero.css('padding-top', jQuery('.header').outerHeight() + 'px');
-}
+// jQuery(document).ready(function () {
+//     adjust_top_bar_spacer();
+//
+// });
+//
+// jQuery(window).resize(function () {
+//     adjust_top_bar_spacer();
+// });
+//
+// function adjust_top_bar_spacer() {
+//     // todo: this should be more dynamically chosen
+//     let hero = jQuery('.hero');
+//     hero.css('padding-top', jQuery('.header').outerHeight() + 'px');
+// }
